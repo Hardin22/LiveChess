604bf80b4d226224e50c11e59710404be273d3ea
Francesco Albano <francesco.albano@studenti.unina.it>
2026-05-08 10:52:03 +0200

    Initial Commit

---
+++ LiveChess/AppModel.swift
@@ -0,0 +1,21 @@
+//
+//  AppModel.swift
+//  LiveChess
+//
+//  Created by Francesco Albano on 08/05/26.
+//
+
+import SwiftUI
+
+/// Maintains app-wide state
+@MainActor
+@Observable
+class AppModel {
+    let immersiveSpaceID = "ImmersiveSpace"
+    enum ImmersiveSpaceState {
+        case closed
+        case inTransition
+        case open
+    }
+    var immersiveSpaceState = ImmersiveSpaceState.closed
+}
---
+++ LiveChess/ImmersiveView.swift
@@ -0,0 +1,30 @@
+//
+//  ImmersiveView.swift
+//  LiveChess
+//
+//  Created by Francesco Albano on 08/05/26.
+//
+
+import SwiftUI
+import RealityKit
+import RealityKitContent
+
+struct ImmersiveView: View {
+
+    var body: some View {
+        RealityView { content in
+            // Add the initial RealityKit content
+            if let immersiveContentEntity = try? await Entity(named: "Immersive", in: realityKitContentBundle) {
+                content.add(immersiveContentEntity)
+
+                // Put skybox here.  See example in World project available at
+                // https://developer.apple.com/
+            }
+        }
+    }
+}
+
+#Preview(immersionStyle: .mixed) {
+    ImmersiveView()
+        .environment(AppModel())
+}
